7a978698b5cd3ab307e466a0afb71ebe4a51b203
orlando <orlandod543@gmail.com>
2018-10-18 00:50:32 +0200

    Main function adquiring and saving data on txt files

--- main.py
+++ main.py
@@ -9,6 +9,7 @@
 import filter
 from time import localtime, strftime
 import sys, traceback
+import pendrive
 
 #Configuration section. Define here global variables or settings
 #Define filter serial port parameters
@@ -21,15 +22,27 @@ f = filter.air_filter(port, baudrate, timeout=10)
 f.air_filter_start()
 f.reset_input_buffer()
 f.flushInput()
+#create an object pendrive. ToDo. Refactorize and fix this class.
+p = pendrive.pendrive(datalog_path)
 
 #From this point the program start
 
 #The filter waits until a line of data arrives to the serial port and timestamp it
 try:
     while True:
+        #get and timestamp the data
         data =  f.get_data() #get the filter data
         strtime = strftime("%y-%m-%d-%H:%M:%S", localtime()) #get the local time
         data.insert(0,strtime) #append the time to the data
+
+        #define the filename and attempt to write the data
+        filename = strtime[:8]+'.txt'
+        if not p.file_exists(filename):
+            print "Creating file " + str(filename)
+        #map function applies str() to convert strings from float to the list
+        #of data.
+        p.write_append(filename,' '.join(map(str,data))+"\n")
+
         print data
 except KeyboardInterrupt: #Clean the code if the program is keyboard interrupted
     print f.filter_close()
