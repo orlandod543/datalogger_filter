5684f6fbd0c90abd5fbe40ca6eb0d133385cc4c3
orlando <orlandod543@gmail.com>
2018-10-18 00:25:44 +0200

    Add infinite loop to gather the filter data

--- main.py
+++ main.py
@@ -5,21 +5,32 @@
 #and it will upload the file to dropbox
 #The example string is as follows:
 #-> radio = $i          Concentration = 0
-
-
-
-
+#import section. Define here all clases to use
 import filter
+from time import localtime, strftime
+import sys, traceback
 
+#Configuration section. Define here global variables or settings
 #Define filter serial port parameters
-port = "/dev/ttyACM1"
+port = "/dev/ttyACM0"
 baudrate = 9600
-
-#create a filter object
+datalog_path = "data/"
+#Setup section. Define here all the objects to use and configure them.
+#create and initialize the filter object
 f = filter.air_filter(port, baudrate, timeout=10)
-print f.air_filter_start()
-print f.get_name()
-for i in range (0,10):
-    print f.get_data()
+f.air_filter_start()
+f.reset_input_buffer()
+f.flushInput()
 
-print f.filter_close()
+#From this point the program start
+
+#The filter waits until a line of data arrives to the serial port and timestamp it
+try:
+    while True:
+        data =  f.get_data() #get the filter data
+        strtime = strftime("%y-%m-%d-%H:%M:%S", localtime()) #get the local time
+        data.insert(0,strtime) #append the time to the data
+        print data
+except KeyboardInterrupt: #Clean the code if the program is keyboard interrupted
+    print f.filter_close()
+    sys.exit()
