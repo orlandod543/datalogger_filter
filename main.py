75e9a997e1cf4ae360909e88e1f951e4522b2f7c
orlando <orlandod543@gmail.com>
2018-10-18 22:05:55 +0200

    Datalogger starts uploading data onto Dropbox. TODO: - Implement exception handler onto dropbox objects - Implement exception handler onto file objects - Refactorize code - Implement finding of the filter serial port

--- main.py
+++ main.py
@@ -10,12 +10,18 @@ import filter
 from time import localtime, strftime
 import sys, traceback
 import pendrive
+import dropbox
+import os
+from dropbox.files import WriteMode
 
 #Configuration section. Define here global variables or settings
 #Define filter serial port parameters
 port = "/dev/ttyACM0"
 baudrate = 9600
 datalog_path = "data/"
+PD_db_access_token = 'QL-hU5_KShUAAAAAAAALMCIFlNcHRN-GQQOA3PvGtaShc_EPlakjUhyJD026tmLT'
+PD_db_folder = "/mnt/sda1/"
+
 #Setup section. Define here all the objects to use and configure them.
 #create and initialize the filter object
 f = filter.air_filter(port, baudrate, timeout=10)
@@ -24,6 +30,8 @@ f.reset_input_buffer()
 f.flushInput()
 #create an object pendrive. ToDo. Refactorize and fix this class.
 p = pendrive.pendrive(datalog_path)
+PD_dropbox = dropbox.Dropbox(PD_db_access_token)
+print PD_dropbox.users_get_current_account()
 
 #From this point the program start
 
@@ -42,6 +50,13 @@ try:
         #map function applies str() to convert strings from float to the list
         #of data.
         p.write_append(filename,' '.join(map(str,data))+"\n")
+        #Upload data onto dropbox
+        file = open(datalog_path+filename,"r")#open file to upload
+        contents = file.read()
+        PD_dropbox.files_upload(contents,
+        PD_db_folder+filename,
+        mode = WriteMode('overwrite'))
+        file.close()
 
         print data
 except KeyboardInterrupt: #Clean the code if the program is keyboard interrupted
