8b5778cbb6aecab4262a5e8480ce66be756f27a6
orlando <orlandod543@gmail.com>
2018-10-30 00:03:30 +0100

    Modify Main to test the function collectfilteredata:

--- main.py
+++ main.py
@@ -23,32 +23,43 @@ import DataloggerFunctions
 
 
 """Configuration section. Define here global variables or settings"""
-baudrate = 9600
+#sensor section
+Sensortimeout = 60 #time window in seconds to wait for new data
+Sensordatasesize = 2#size of the dataset data the sensor delivers
+Sensorbaudrate = 9600
+#File section
 datalog_path = "data/"
-DBAccessTokenList = {'DBOrlando' : 'QL-hU5_KShUAAAAAAAALMCIFlNcHRN-GQQOA3PvGtaShc_EPlakjUhyJD026tmLT',
-                     'DBPulseDynamics' :'oEgHuggfnPAAAAAAAAACOOkjlzybdPgSd8ZPWHkxqUA9d-bnthhTaY_BSJiZoX5D'}
-PD_db_folder = "/Filterdata/"
-dropbox_user_agent = "Filter"
 file_header = "YY-MM-DD-HH:MM:SS Ratio[%] Concentration[pcs/L]\n"
 
+
+#Dropboxsection
+DBAccessTokenList = {'DBOrlando' : 'QL-hU5_KShUAAAAAAAALMCIFlNcHRN-GQQOA3PvGtaShc_EPlakjUhyJD026tmLT'
+#                    ,'DBPulseDynamics' :'oEgHuggfnPAAAAAAAAACOOkjlzybdPgSd8ZPWHkxqUA9d-bnthhTaY_BSJiZoX5D'
+                     }
+PD_db_folder = "/Filterdata/"
+dropbox_user_agent = "Filter"
+
+#Datalogger section
+TimeWindow = 60#the time window to filter the data in seconds
 
 """Setup section. Define here all the objects to use and configure them."""
 #Check if there is an arduino connected. If there is no, it stops
 #the software
 print "-----Dust Sensor Datalogger-----"
 print "Attempting connection with the arduino"
-port = DataloggerFunctions.RetrieveARduinoCOMport()
-if not port:
+SensorPort = DataloggerFunctions.RetrieveARduinoCOMport()
+if not SensorPort:
     print "There is no Arduino available"
     sys.exit(1)
 else:
-    print "Found Arduino on port"+ str(port)
+    print "Found Arduino on port"+ str(SensorPort)
 
 #create and initialize the filter object
 #Note: Set the timeout to be bigger than sample time. for now is 60 seconds.
-f = filter.air_filter(port,
-                    baudrate,
-                    timeout=60) #Set the timeout to a bigger time than the sample
+f = filter.air_filter(SensorPort,
+                    Sensorbaudrate,
+                    timeout=Sensortimeout,#Set the timeout to a bigger time than the sample
+                    datanumber = Sensordatasesize)
 f.air_filter_start()
 
 #initialize the pendrive
@@ -65,7 +76,7 @@ print "Starting logging data:"
 try:
     while True:
         #get and timestamp the data
-        data = DataloggerFunctions.CollectSensorData(f) #collect data logger data
+        data = DataloggerFunctions.CollectFilteredData(f,TimeWindow) #collect filtered datalogger data
         filename = data[0][:8]+'.txt' #set the name of the file by the data
 
         #For new files, the file header is writen at the beginning.
@@ -78,7 +89,7 @@ try:
         #Upload data onto dropbox
         filepath = datalog_path+filename
         dbfilepath = PD_db_folder+filename
-        DataloggerFunctions.UploadFileToDropboxUsers(filepath, dbfilepath, DBSessionObjects)
+        #DataloggerFunctions.UploadFileToDropboxUsers(filepath, dbfilepath, DBSessionObjects)
         print data
 
 except KeyboardInterrupt: #Clean the code if the program is keyboard interrupted
